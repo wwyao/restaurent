a3f559a1f225f4796f59a90d290cac266823cb33
wyao <708919996@qq.com>
2017-01-17 20:34:14 +0800

    20170117

---
+++ checkName.php
@@ -0,0 +1,21 @@
+<?php
+header("Content-Type: text/html;charset=utf-8");
+$username = $_GET['username'];
+
+$conn = mysqli_connect('localhost','root','root','restaurent');
+if($conn->connect_error){
+	echo "链接服务器错误";
+}else{
+	$conn->query = "set names utf8";
+	$sql = "select * from Users where username='$username'";
+	$conn->query($sql);
+	$result = new stdClass();
+	if(mysqli_affected_rows($conn)){
+		$result->result = 1;
+	}else{
+		$result->result = 0;
+	}
+	echo json_encode($result);
+
+}
+?>
